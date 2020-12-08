320e10e4122c67773c4892904f1fbdfffa2347af
Ruudzaki <56171632+Ruudzaki@users.noreply.github.com>
2020-12-08 19:44:54 +0200

    add webdriver automation

---
+++ AutomationRoot/Automation.Testing/SeleniumSamples.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+namespace Automation.Testing
+{
+    [TestClass]
+    public class SeleniumSamples
+    {
+        [TestMethod]
+        public void WebDriverSamples()
+        {
+            IWebDriver driver = new ChromeDriver();
+            Thread.Sleep(1000);
+            driver.Dispose();
+
+            driver = new FirefoxDriver();
+            Thread.Sleep(1000);
+            driver.Dispose();
+
+            driver = new InternetExplorerDriver();
+            Thread.Sleep(1000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void WebElementSamples()
+        {
+            var driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            
+            driver.Navigate().GoToUrl("https://gravitymvctestapplication.azurewebsites.net/");
+            driver.FindElement(By.XPath("//a[.='Students']")).Click();
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void SelectElementSamples()
+        {
+            var driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("https://gravitymvctestapplication.azurewebsites.net/Course/");
+            var element = driver.FindElement(By.XPath("//select[@id='SelectedDepartment']"));
+            var selectElement = new SelectElement(element);
+            selectElement.SelectByValue("4");
+            driver.FindElement(By.XPath("//input[@value='Filter']")).Click();
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+    }
+}
