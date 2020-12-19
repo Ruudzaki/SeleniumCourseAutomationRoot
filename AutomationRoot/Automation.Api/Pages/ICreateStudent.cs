94ac9ce7dd7a98b95be1cfb23a9d9b5b1eb9a9ec
Ruudzaki <kostybelous@gmail.com>
2020-12-19 10:37:56 +0200

    create interfaces as components and pages of Students page

---
+++ AutomationRoot/Automation.Api/Pages/ICreateStudent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Automation.Api.Components;
+
+namespace Automation.Api.Pages
+{
+    public interface ICreateStudent : IStudentDetails, ICreate<IStudents>, IBack<IStudents>
+    {
+
+    }
+}
---
+++ AutomationRoot/Automation.Api/Pages/IStudents.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Automation.Api.Components;
+
+namespace Automation.Api.Pages
+{
+    public interface IStudents : IPageNavigator<IStudents>, IMenu
+    {
+        IStudents FindByName(string name);
+        IEnumerable<IStudent> Students();
+    }
+}
