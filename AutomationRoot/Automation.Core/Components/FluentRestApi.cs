65dcfe042b4386a66bca3b902b410114b87e62a2
Ruudzaki <56171632+Ruudzaki@users.noreply.github.com>
2020-12-24 15:42:11 +0200

    add FluentRestApi class

---
+++ AutomationRoot/Automation.Core/Components/FluentRestApi.cs
@@ -0,0 +1,47 @@
+﻿using Automation.Core.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation.Core.Components
+{
+    class FluentRestApi : IFluent
+    {
+        protected FluentRestApi(HttpClient httpClient, ILogger logger)
+        {
+            HttpClient = httpClient;
+            Logger = logger;
+        }
+
+        protected FluentRestApi(HttpClient httpClient)
+            : this(httpClient, new TraceLogger())
+        {
+        }
+
+        public HttpClient HttpClient { get; }
+        public ILogger Logger { get; }
+
+        public T ChangeContext<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T ChangeContext<T>(ILogger logger)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T ChangeContext<T>(string application)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T ChangeContext<T>(string application, ILogger logger)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
